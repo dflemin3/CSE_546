db959317b67202b93c58289363f51551b90b5dfc
David Fleming <dflemin3@uw.edu>
2016-11-14 15:10:09 -0800

    HW3: added more kmeans results

--- HW3/hw3_5.1.py
+++ HW3/hw3_5.1.py
@@ -17,89 +17,172 @@ sys.path.append("..")
 import DML.data_processing.mnist_utils as mu
 import DML.validation.validation as val
 import DML.clustering.kmeans as kmeans
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 
 
 # Flags to control functionality
-show_plots = False
-save_plots = False
-k = 16
-verbose = True
+show_plots = True
+save_plots = True
+run_k_16 = False
+run_k_250 = True
 
 # Load in MNIST data
 print("Loading MNIST data...")
-X_train, y_train = mu.load_mnist(dataset='testing')
-X_test, y_test = mu.load_mnist(dataset='training')
+X_train, y_train = mu.load_mnist(dataset='training')
+X_test, y_test = mu.load_mnist(dataset='testing')
 
-print("Running k-means...")
-y_cluster, mu_hat, rec_err, iter_arr = kmeans.kmeans(X_train, k=k, verbose=verbose)
+###################################################
+#
+# Run kmeans with 16 clusters
+#
+###################################################
 
-# Now classifiy
-classifier = kmeans.k_mapper(y_cluster, y_train)
-y_hat_train = kmeans.k_classify(X_train, mu_hat, classifier)
-y_hat_test = kmeans.k_classify(X_test, mu_hat, classifier)
+if run_k_16:
 
-print(val.loss_01(y_train,y_hat_train))
-print(val.loss_01(y_test,y_hat_test))
+    k = 16
+    verbose = True
 
-# Compute number of assignments in descending order
-counts = np.bincount(y_cluster.squeeze())
-x_counts = np.arange(k)
+    print("Running k-means with %d clusters..." % k)
+    y_cluster, mu_hat, rec_err, iter_arr = kmeans.kmeans(X_train, k=k, verbose=verbose)
 
-# Sort in descending order
-inds = np.argsort(counts)[::-1]
+    # Compute number of assignments in descending order
+    # Occurences as function of cluster number
+    counts = np.bincount(y_cluster.squeeze())
+    x_counts = np.arange(k)
 
-if show_plots:
+    # Sort in descending order
+    inds = np.argsort(counts)[::-1]
+
     # Plot number of assignments for each center in descending order
+    if show_plots:
 
-    fig, ax = plt.subplots()
+        fig, ax = plt.subplots()
 
-    ax.plot(counts[inds],"o")
-    plt.locator_params(axis='x',nbins=k)
-    #ax.xaxis.set_ticks(x_counts[inds])
-    ax.set_xticklabels(x_counts[inds])
+        ax.plot(counts[inds],"o")
+        plt.locator_params(axis='x',nbins=k)
+        #ax.xaxis.set_ticks(x_counts[inds])
+        ax.set_xticklabels(x_counts[inds])
 
-    ax.set_xlabel("Cluster Number")
-    ax.set_ylabel("Number of Assignments")
+        ax.set_xlabel("Cluster Number")
+        ax.set_ylabel("Number of Assignments")
 
-    plt.show()
+        plt.show()
 
-    if save_plots:
-        fig.savefig("k_16_num_assignments.pdf")
+        if save_plots:
+            fig.savefig("k_16_num_assignments.pdf")
 
-if show_plots:
     # Plot the reconstruction error vs iteration
-    fig, ax = plt.subplots()
+    if show_plots:
+        fig, ax = plt.subplots()
 
-    ax.plot(iter_arr,rec_err,"o-",lw=2,color="blue")
+        ax.plot(iter_arr,rec_err,"o-",lw=2,color="blue")
 
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel("Mean Reconstruction Error")
+        ax.set_xlabel("Iteration")
+        ax.set_ylabel("Mean Reconstruction Error")
 
-    plt.show()
+        plt.show()
 
-    if save_plots:
-        fig.savefig("k_16_rec_err.pdf")
+        if save_plots:
+            fig.savefig("k_16_rec_err.pdf")
 
-if show_plots:
-    fig, axes = plt.subplots(nrows=4,ncols=4)
+    # Plot the centroids according to frequency in which they were assigned
+    if show_plots:
+        fig, axes = plt.subplots(nrows=4,ncols=4)
 
-    # Plot the centroids!
-    for ii, ax in enumerate(axes.flatten()):
+        for ii, ax in enumerate(axes.flatten()):
 
-        # Diplay ith centroid
-        image = mu_hat[inds[ii],:]
+            # Diplay ith centroid
+            image = mu_hat[inds[ii],:]
 
-        # Plot reconstructed image
-        ax.imshow(image.reshape((28, 28)), cmap='binary', interpolation="nearest")
-        ax.text(0.95, 0.05, 'n = %d' % counts[inds[ii]], ha='right',
-                transform=ax.transAxes, color='red')
-        ax.set_xticks([])
-        ax.set_yticks([])
+            # Plot reconstructed image
+            ax.imshow(image.reshape((28, 28)), cmap='binary', interpolation="nearest")
+            ax.text(0.95, 0.05, 'n = %d' % counts[inds[ii]], ha='right',
+                    transform=ax.transAxes, color='red')
+            ax.set_xticks([])
+            ax.set_yticks([])
 
-    fig.tight_layout()
-    plt.show()
+        fig.tight_layout()
+        plt.show()
 
-    if save_plots:
-        fig.savefig("k_16_means.pdf")
+        if save_plots:
+            fig.savefig("k_16_means.pdf")
+
+###################################################
+#
+# Run kmeans with 250 clusters
+#
+###################################################
+
+if run_k_250:
+
+    k = 250
+    verbose = True
+
+    print("Running k-means with %d clusters..." % k)
+    y_cluster, mu_hat, rec_err, iter_arr = kmeans.kmeans(X_train, k=k, verbose=verbose)
+
+    # Compute number of assignments in descending order
+    # Occurences as function of cluster number
+    counts = np.bincount(y_cluster.squeeze())
+    x_counts = np.arange(k)
+
+    # Sort in descending order
+    inds = np.argsort(counts)[::-1]
+
+    # Plot number of assignments for each center in descending order
+    if show_plots:
+
+        fig, ax = plt.subplots(figsize=(24,8))
+
+        ax.plot(counts[inds],"o")
+
+        # Do stuff to make x-axis tick labels actually visible
+        plt.locator_params(axis='x',nbins=k)
+        ax.set_xticklabels(x_counts[inds])
+        ax.set_xticklabels(ax.xaxis.get_majorticklabels(), rotation=90)
+        for tick in ax.xaxis.get_major_ticks():
+                tick.label.set_fontsize(4)
+
+        ax.set_xlabel("Cluster Number")
+        ax.set_ylabel("Number of Assignments")
+
+        plt.show()
+
+        if save_plots:
+            fig.savefig("k_250_num_assignments.pdf")
+
+    # Plot the reconstruction error vs iteration
+    if show_plots:
+        fig, ax = plt.subplots()
+
+        ax.plot(iter_arr,rec_err,"o-",lw=2,color="blue")
+
+        ax.set_xlabel("Iteration")
+        ax.set_ylabel("Mean Reconstruction Error")
+
+        plt.show()
+
+        if save_plots:
+            fig.savefig("k_250_rec_err.pdf")
+
+    # Plot the centroids according to frequency in which they were assigned
+    if show_plots:
+        fig, axes = plt.subplots(nrows=4,ncols=4)
+
+        for ii, ax in enumerate(axes.flatten()):
+
+            # Diplay ith centroid
+            image = mu_hat[inds[ii],:]
+
+            # Plot reconstructed image
+            ax.imshow(image.reshape((28, 28)), cmap='binary', interpolation="nearest")
+            ax.text(0.95, 0.05, 'n = %d' % counts[inds[ii]], ha='right',
+                    transform=ax.transAxes, color='red')
+            ax.set_xticks([])
+            ax.set_yticks([])
+
+        fig.tight_layout()
+        plt.show()
+
+        if save_plots:
+            fig.savefig("k_250_means.pdf")
